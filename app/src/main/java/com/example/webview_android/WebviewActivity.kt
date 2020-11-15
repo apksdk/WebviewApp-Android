414b0362af0caf0c2bf1b5eb25fa3d962e2b2d48
Joshua Wu <26704772+apksdk@users.noreply.github.com>
2020-11-15 19:24:17 +1100

    load url support

--- app/src/main/java/com/example/webview_android/WebviewActivity.kt
+++ app/src/main/java/com/example/webview_android/WebviewActivity.kt
@@ -1,18 +1,23 @@
 package com.example.webview_android
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.webkit.WebView
+import android.webkit.WebViewClient
 import android.widget.EditText
 
 class WebviewActivity : AppCompatActivity() {
     private lateinit var webview: WebView
 
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_webview)
-        webview = findViewById<WebView>(R.id.webview);
+        webview = findViewById<WebView>(R.id.webview)
         val webURL = intent.getStringExtra("webURL")
+        webview.settings.javaScriptEnabled = true
+        webview.webViewClient = WebViewClient()
         webview.loadUrl(webURL.toString())
     }
 }
